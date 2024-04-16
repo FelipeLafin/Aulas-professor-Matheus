c8823cc52777c962a7c2864fc8313c4a2ff04837
Felipe Lafin Haushahn <164341391+FelipeLafin@users.noreply.github.com>
2024-04-16 11:25:27 -0300

    pastas organizadas

---
+++ Mateus/2024_04_16/calculo.py
@@ -0,0 +1,5 @@
+digite a largura: 5
+digite a altura: 20
+Area:  100
+Perímetro:  50
+Hipotenusa:  20.615528128088304
---
+++ Mateus/2024_04_16/codigo calculo.py
@@ -0,0 +1,11 @@
+largura = int(input('digite a largura: '))
+altura = int(input('digite a altura: '))
+
+area = altura * largura
+print('Area: ', area)
+
+perimetro = 2 * (largura + altura)
+print('Perímetro: ', perimetro)
+
+hipotenusa = (largura ** 2 + altura ** 2) ** (1/2)
+print('Hipotenusa: ', hipotenusa)
