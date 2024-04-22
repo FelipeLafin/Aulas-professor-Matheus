000ae7f19e26efd93a9db9c1dbeea1f6152c3aed
Felipe Lafin Haushahn <164341391+FelipeLafin@users.noreply.github.com>
2024-04-22 10:56:02 -0300

    horóscopo

---
+++ Mateus/2024_04_22/Horóscopo.py
@@ -0,0 +1,45 @@
+print('Quando um programador nasce, atribuímos um signo para o indivíduo')
+mes = input('Informe o mês que você nasceu para ver seu signo: ')
+if mes == 'janeiro':
+        print('Signo = Python')
+elif mes == 'fevereiro':
+        print('Signo = Fortran')
+elif mes == 'março':
+        print('Signo = Rust')
+elif mes == 'abril':
+        print('Signo = Go')
+elif mes == 'maio':
+        print('Signo = Java')
+elif mes == 'junho':
+        print('Signo = JavaScript')
+elif mes == 'julho':
+        print('Signo = C')
+elif mes == 'agosto':
+        print('Signo = PHP')
+elif mes == 'setembro':
+        print('Signo = Haskell')
+elif mes == 'outubro':
+        print('Signo = Perl')
+elif mes == 'novembro':
+        print('Signo = Swift')
+elif mes == 'dezembro':
+        print('Signo = Kotlin')
+else:
+        print('mês inválido')
+print('')
+print('----------------------------------------------------------------------------------------------------------------')
+print('')
+print('Lista de signos:')
+print('')
+print('janeiro = Python')
+print('fevereiro = Fortran')
+print('março = Rust')
+print('abril = Go')
+print('maio = Java')
+print('junho = JavaScript')
+print('julho = C')
+print('agosto = PHP')
+print('setembro = Haskell')
+print('outubro = Perl')
+print('novembro = Swift')
+print('dezembro = Kotlin')
